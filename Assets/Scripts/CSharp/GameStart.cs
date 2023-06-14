9c1a9df2bc21f5d1bd99ea049d55e89f64d5c438
unknown <wb.yanqihua@game.ntes>
2023-06-14 14:08:18 +0800

    ecsl

--- Assets/Scripts/CSharp/GameStart.cs
+++ Assets/Scripts/CSharp/GameStart.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -6,15 +7,19 @@ using XLua;
 
 public class GameStart : MonoBehaviour
 {
-    LuaEnv luaEnv;
+    internal static LuaEnv luaEnv = new LuaEnv();
+    internal static float lastGCTime = 0;
+    internal const float GCInterval = 1;
+    private Action luaUpdate;
+    private LuaTable scriptEnv;
 
     void Awake()
     {
-        luaEnv = new LuaEnv();
+        DontDestroyOnLoad(this);
+
         luaEnv.AddLoader((ref string name) => {
             name = name.Replace(".","/");
             var path = string.Format("{0}/Scripts/Lua/{1}.lua",Application.dataPath,name);
-            // Debug.Log("require路径:"+path);
             if(File.Exists(path))
             {
                 return File.ReadAllBytes(path);
@@ -22,18 +27,36 @@ public class GameStart : MonoBehaviour
             return null;
         });
         luaEnv.DoString("require('Core.Main')");
+        
+        scriptEnv = luaEnv.NewTable();
+
+        // 为每个脚本设置一个独立的环境，可一定程度上防止脚本间全局变量、函数冲突
+        LuaTable meta = luaEnv.NewTable();
+        meta.Set("__index", luaEnv.Global);
+        scriptEnv.SetMetaTable(meta);
+        meta.Dispose();
+
+        scriptEnv.Set("self", this);
+        scriptEnv.Get("Update", out luaUpdate);
     }
 
     void Update()
     {
-        if (luaEnv != null)
+        if (luaUpdate != null)
+        {
+            luaUpdate();
+        }
+        if (Time.time - lastGCTime > GCInterval)
         {
             luaEnv.Tick();
+            lastGCTime = Time.time;
         }
     }
 
     void Destroy()
     {
+        luaUpdate = null;
+        scriptEnv.Dispose();
         luaEnv.Dispose();
     }
 }
