ba76846223e0022b3c660fdc2d49a18e408b881d
unknown <wb.yanqihua@game.ntes>
2023-08-10 10:52:37 +0800

    fixmeta

---
+++ Assets/XLua/Gen/FileUtilsWrap.cs
@@ -0,0 +1,246 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System.Collections.Generic;
+
+
+namespace XLua.CSObjectWrap
+{
+    using Utils = XLua.Utils;
+    public class FileUtilsWrap 
+    {
+        public static void __Register(RealStatePtr L)
+        {
+			ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+			System.Type type = typeof(FileUtils);
+			Utils.BeginObjectRegister(type, L, translator, 0, 0, 0, 0);
+			
+			
+			
+			
+			
+			
+			Utils.EndObjectRegister(type, L, translator, null, null,
+			    null, null, null);
+
+		    Utils.BeginClassRegister(type, L, __CreateInstance, 6, 0, 0);
+			Utils.RegisterFunc(L, Utils.CLS_IDX, "GetAllFile", _m_GetAllFile_xlua_st_);
+            Utils.RegisterFunc(L, Utils.CLS_IDX, "GetCurrentDir", _m_GetCurrentDir_xlua_st_);
+            Utils.RegisterFunc(L, Utils.CLS_IDX, "CopyFloder", _m_CopyFloder_xlua_st_);
+            Utils.RegisterFunc(L, Utils.CLS_IDX, "FormatFilePath", _m_FormatFilePath_xlua_st_);
+            Utils.RegisterFunc(L, Utils.CLS_IDX, "CreateFolder", _m_CreateFolder_xlua_st_);
+            
+			
+            
+			
+			
+			
+			Utils.EndClassRegister(type, L, translator);
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int __CreateInstance(RealStatePtr L)
+        {
+            
+			try {
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+				if(LuaAPI.lua_gettop(L) == 1)
+				{
+					
+					var gen_ret = new FileUtils();
+					translator.Push(L, gen_ret);
+                    
+					return 1;
+				}
+				
+			}
+			catch(System.Exception gen_e) {
+				return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+			}
+            return LuaAPI.luaL_error(L, "invalid arguments to FileUtils constructor!");
+            
+        }
+        
+		
+        
+		
+        
+        
+        
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_GetAllFile_xlua_st_(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+            
+                
+                {
+                    string _dir = LuaAPI.lua_tostring(L, 1);
+                    string _pattern = LuaAPI.lua_tostring(L, 2);
+                    
+                        var gen_ret = FileUtils.GetAllFile( _dir, _pattern );
+                        translator.Push(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_GetCurrentDir_xlua_st_(RealStatePtr L)
+        {
+		    try {
+            
+            
+            
+                
+                {
+                    
+                        var gen_ret = FileUtils.GetCurrentDir(  );
+                        LuaAPI.lua_pushstring(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_CopyFloder_xlua_st_(RealStatePtr L)
+        {
+		    try {
+            
+                ObjectTranslator translator = ObjectTranslatorPool.Instance.Find(L);
+            
+            
+            
+			    int gen_param_count = LuaAPI.lua_gettop(L);
+            
+                if(gen_param_count == 4&& (LuaAPI.lua_isnil(L, 1) || LuaAPI.lua_type(L, 1) == LuaTypes.LUA_TSTRING)&& (LuaAPI.lua_isnil(L, 2) || LuaAPI.lua_type(L, 2) == LuaTypes.LUA_TSTRING)&& translator.Assignable<System.Func<System.IO.FileInfo, bool>>(L, 3)&& translator.Assignable<System.Func<System.IO.FileInfo, string>>(L, 4)) 
+                {
+                    string _sourceDir = LuaAPI.lua_tostring(L, 1);
+                    string _targetDir = LuaAPI.lua_tostring(L, 2);
+                    System.Func<System.IO.FileInfo, bool> _fileFilter = translator.GetDelegate<System.Func<System.IO.FileInfo, bool>>(L, 3);
+                    System.Func<System.IO.FileInfo, string> _nameGetter = translator.GetDelegate<System.Func<System.IO.FileInfo, string>>(L, 4);
+                    
+                    FileUtils.CopyFloder( _sourceDir, _targetDir, _fileFilter, _nameGetter );
+                    
+                    
+                    
+                    return 0;
+                }
+                if(gen_param_count == 3&& (LuaAPI.lua_isnil(L, 1) || LuaAPI.lua_type(L, 1) == LuaTypes.LUA_TSTRING)&& (LuaAPI.lua_isnil(L, 2) || LuaAPI.lua_type(L, 2) == LuaTypes.LUA_TSTRING)&& translator.Assignable<System.Func<System.IO.FileInfo, bool>>(L, 3)) 
+                {
+                    string _sourceDir = LuaAPI.lua_tostring(L, 1);
+                    string _targetDir = LuaAPI.lua_tostring(L, 2);
+                    System.Func<System.IO.FileInfo, bool> _fileFilter = translator.GetDelegate<System.Func<System.IO.FileInfo, bool>>(L, 3);
+                    
+                    FileUtils.CopyFloder( _sourceDir, _targetDir, _fileFilter );
+                    
+                    
+                    
+                    return 0;
+                }
+                if(gen_param_count == 2&& (LuaAPI.lua_isnil(L, 1) || LuaAPI.lua_type(L, 1) == LuaTypes.LUA_TSTRING)&& (LuaAPI.lua_isnil(L, 2) || LuaAPI.lua_type(L, 2) == LuaTypes.LUA_TSTRING)) 
+                {
+                    string _sourceDir = LuaAPI.lua_tostring(L, 1);
+                    string _targetDir = LuaAPI.lua_tostring(L, 2);
+                    
+                    FileUtils.CopyFloder( _sourceDir, _targetDir );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+            return LuaAPI.luaL_error(L, "invalid arguments to FileUtils.CopyFloder!");
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_FormatFilePath_xlua_st_(RealStatePtr L)
+        {
+		    try {
+            
+            
+            
+                
+                {
+                    string _filePath = LuaAPI.lua_tostring(L, 1);
+                    
+                        var gen_ret = FileUtils.FormatFilePath( _filePath );
+                        LuaAPI.lua_pushstring(L, gen_ret);
+                    
+                    
+                    
+                    return 1;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+        static int _m_CreateFolder_xlua_st_(RealStatePtr L)
+        {
+		    try {
+            
+            
+            
+                
+                {
+                    string _path = LuaAPI.lua_tostring(L, 1);
+                    
+                    FileUtils.CreateFolder( _path );
+                    
+                    
+                    
+                    return 0;
+                }
+                
+            } catch(System.Exception gen_e) {
+                return LuaAPI.luaL_error(L, "c# exception:" + gen_e);
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
+		
+		
+		
+		
+    }
+}
